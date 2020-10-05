f077af6447af76f27b61e75d1516ca376da57434
PramodSaraswat <8441918081pramod@gmail.com>
2020-10-05 19:07:12 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Main from './components/MainComponent';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
 
 class App extends React.Component {
 
 	
-render(){
-	return (
-    <div>
-        <Main />
-    </div>
-  );}
+	render(){
+		return (
+			<BrowserRouter>
+			    <div>
+			        <Main />
+			    </div>
+			</BrowserRouter>
+	  	);
+	}
 }
 
 export default App;
